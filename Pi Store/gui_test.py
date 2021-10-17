fbdf2aec60870e8c0b1538a64609c57975d5e417
lukemccrea26 <90562614+lukemccrea26@users.noreply.github.com>
2021-10-17 13:03:33 -0500

    Add files via upload

---
+++ Pi Store/gui_test.py
@@ -0,0 +1,21 @@
+from guizero import App, Text, TextBox, PushButton, Slider, Picture, Box
+
+app = App(title="Pi Store", layout="grid", width="900")
+
+def search():
+    print(search_input.value)
+    search_input.value = ""
+
+def resize():
+    header.width = app.width
+
+header = Box(app, layout="auto", grid=[1,1,100,1], width="900", height=100, align="left")
+header.bg = "red"
+app.when_resized = resize
+
+logo = Picture(header, image="logo.png", align="left")
+search_input = TextBox(header, width=30, align="right")
+search_button = PushButton(header, command=search, text="Search", align="right")
+
+
+app.display()
